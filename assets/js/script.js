5dd9631e077e2222b184fecbb63072468e2288df
Jelena Jovicic <jejovicic@gmail.com>
2023-02-14 10:30:59 +0100

    styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 const btnSubmit = document.getElementById("submit");
 const modal = document.getElementById("modal");
+const output = document.getElementById("output");
+let rating;
+const buttons = [...document.getElementsByClassName("ratings")];
+buttons.forEach(button => 
+    button.addEventListener("click", (e) => {
+        rating = e.target.value;
+        output.textContent = rating;
+    }));
 
 btnSubmit.addEventListener("click", (e) => {
 modal.showModal();
 })
+
