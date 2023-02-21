f8bbee66b1f01864d16da24f20216efc68f45892
Jelena Jovicic <jejovicic@gmail.com>
2023-02-21 11:16:45 +0100

    mobile styles, readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
 const btnSubmit = document.getElementById("submit");
+const form = document.getElementById("form");
 const modal = document.getElementById("modal");
 const output = document.getElementById("output");
 let rating;
 const buttons = [...document.getElementsByClassName("ratings")];
-buttons.forEach(button => 
-    button.addEventListener("click", (e) => {
-        rating = e.target.value;
-        output.textContent = rating;
-    }));
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    btnSubmit.removeAttribute("disabled");
+    rating = e.target.value;
+    output.textContent = `\xa0${rating}\xa0`;
+  })
+);
 
-btnSubmit.addEventListener("click", (e) => {
-modal.showModal();
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  modal.showModal();
+});
 
+modal.addEventListener("click", () => modal.close()); //click anywhere to close modal
